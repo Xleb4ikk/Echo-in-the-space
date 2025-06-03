155205be6975febfd83b660fe743a31093a37a90
Xleb <129383981+Xleb4ikk@users.noreply.github.com>
2025-06-03 19:48:04 +0300

    Added Darkness

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,12 +4,14 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    public float sprintMultiplier = 1.7f; // 70% быстрее
     public float gravity = -9.81f;  // гравитация
     public float jumpHeight = 1.5f; // опционально для прыжков
 
     private CharacterController controller;
     private InputSystem_Actions inputActions;
     private Vector2 moveInput;
+    private bool isSprinting = false;
 
     private float velocityY = 0f;
     private bool isGrounded;
@@ -24,12 +26,16 @@ public class Player : MonoBehaviour
         inputActions.Player.Enable();
         inputActions.Player.Move.performed += OnMove;
         inputActions.Player.Move.canceled += OnMove;
+        inputActions.Player.Sprint.performed += OnSprint;
+        inputActions.Player.Sprint.canceled += OnSprint;
     }
 
     void OnDisable()
     {
         inputActions.Player.Move.performed -= OnMove;
         inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Sprint.performed -= OnSprint;
+        inputActions.Player.Sprint.canceled -= OnSprint;
         inputActions.Player.Disable();
     }
 
@@ -55,12 +61,20 @@ public class Player : MonoBehaviour
         // Добавляем вертикальное движение
         move.y = velocityY;
 
+        // Выбираем скорость: обычная или спринт
+        float currentSpeed = isSprinting ? speed * sprintMultiplier : speed;
+
         // Двигаем контроллер
-        controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * currentSpeed * Time.deltaTime);
     }
 
     private void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = context.ReadValueAsButton();
     }
 }
