21619ddd0e37021b52c89d8806fb565a3d4a24e9
Xleb <129383981+Xleb4ikk@users.noreply.github.com>
2025-06-04 21:58:08 +0300

    Fix scripts and lights

---
+++ Assets/Scripts/Map/LightController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    [SerializeField] private GameObject mapLight; // Ссылка на объект Map light
+    private Light[] childLights; // Массив для хранения всех дочерних источников света
+
+    private void Start()
+    {
+        // Получаем все компоненты Light у дочерних объектов
+        if (mapLight != null)
+        {
+            childLights = mapLight.GetComponentsInChildren<Light>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Проверяем, вошел ли игрок в триггер
+        if (other.CompareTag("Player"))
+        {
+            // Выключаем все дочерние источники света
+            foreach (Light light in childLights)
+            {
+                light.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Проверяем, вышел ли игрок из триггера
+        if (other.CompareTag("Player"))
+        {
+            // Включаем все дочерние источники света
+            foreach (Light light in childLights)
+            {
+                light.enabled = true;
+            }
+        }
+    }
+} 
