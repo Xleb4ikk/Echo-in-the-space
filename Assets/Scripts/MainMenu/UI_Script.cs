69babf3558ccd7f74cb9393b9bde81c534def322
Fodicat <angel.v.lastah@gmail.com>
2025-06-05 21:00:55 +0300

    Update MainMenu

--- Assets/Scripts/MainMenu/UI_Script.cs
+++ Assets/Scripts/MainMenu/UI_Script.cs
@@ -23,13 +23,18 @@ public class UI_Script : MonoBehaviour
 
     [Header("Ui")]
     public Image Darkening;
+    public CanvasGroup Canvas;
 
     [Header("Настройки начала игры")]
     public float WaitingDarkening = 2f;
     public float durationDarkening = 3f;
     public float transparentDarkening = 255f;
 
-    private bool DarkeningStart = false;
+    [Header("Настройки запуска игры")]
+    public float WaitfadeDurationIn = 1f;
+    public float fadeDurationIn = 1.5f;
+    public float WaitfadeDurationOut = 1f;
+    public float fadeDurationOut = 1f;
 
     void Start()
     {
@@ -38,6 +43,12 @@ public class UI_Script : MonoBehaviour
         AddHoverHandler(NewGame_Button.gameObject, NewGame_Button_Text);
         AddHoverHandler(Settings_Button.gameObject, Settings_Button_Text);
         AddHoverHandler(Exit_Button.gameObject, Exit_Button_Text);
+
+        Canvas.alpha = 0f;
+        Canvas.interactable = false;
+        Canvas.blocksRaycasts = false;
+
+        StartCoroutine(FadeIn());
     }
 
     void Update()
@@ -49,6 +60,7 @@ public class UI_Script : MonoBehaviour
     {
         ship_script.StartNewGameAnimation();
         StartCoroutine(WaitDarkening());
+        StartCoroutine(FadeOut());
     }
 
     private void StartChangeScene()
@@ -60,6 +72,42 @@ public class UI_Script : MonoBehaviour
     {
         var trigger = uiObject.AddComponent<PointerHoverHandler>();
         trigger.targetText = linkedText;
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float timer = 0f;
+
+        yield return new WaitForSeconds(WaitfadeDurationIn);
+
+        while (timer < fadeDurationIn)
+        {
+            timer += Time.deltaTime;
+            Canvas.alpha = Mathf.Clamp01(timer / fadeDurationIn);
+            yield return null;
+        }
+
+        Canvas.interactable = true;
+        Canvas.blocksRaycasts = true;
+    }
+
+    public IEnumerator FadeOut()
+    {
+        float time = 0f;
+
+        Canvas.interactable = false;
+        Canvas.blocksRaycasts = false;
+
+        yield return new WaitForSeconds(WaitfadeDurationOut);
+
+        while (time < WaitfadeDurationOut)
+        {
+            time += Time.deltaTime;
+            Canvas.alpha = Mathf.Clamp01(1f - (time / WaitfadeDurationOut));
+            yield return null;
+        }
+
+        Canvas.alpha = 0f;
     }
 
     IEnumerator WaitDarkening()
@@ -76,6 +124,7 @@ public class UI_Script : MonoBehaviour
         {
             timer += Time.deltaTime;
             float alpha = Mathf.Clamp01(timer / durationDarkening);
+            Debug.Log(alpha);
             Darkening.color = new Color(color.r, color.g, color.b, alpha);
             yield return null;
         }
