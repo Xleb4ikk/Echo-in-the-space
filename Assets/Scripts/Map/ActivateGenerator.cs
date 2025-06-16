15aa39df18a3011c2938576ed594b35b626e1632
Fodicat <angel.v.lastah@gmail.com>
2025-06-17 00:34:53 +0300

    Add generator sounds

--- Assets/Scripts/Map/ActivateGenerator.cs
+++ Assets/Scripts/Map/ActivateGenerator.cs
@@ -7,28 +7,59 @@ using UnityEngine.Playables;
 
 public class ActivateGenerator : MonoBehaviour
 {
-    public Collider triggerZone;
-    public Transform playerTransform;
-    public Transform MonsterTransform;
-    public Animator ButtonAnimation;
-    public PlayableDirector director;
-    public GameObject Camera;
-    public Renderer ButtonMaterial;
-    public Material newMaterial;
-    public PlayerCamera PlayerCameraScript;
-    public Player PlayerMovementScript;
-    public TeleportOnKey TeleportOnKeyPlayer;
-    public CameraFade CameraFade;
-    public GameObject FakeMonster;
-    public GameObject RealMonster;
-    public GameObject AnimationDoor;
-    public GameObject RealDoor;
-    public GameObject DialogTriger;
-    public List<GameObject> VisibleWalls;
+    [Header("Анимации")]
+    [SerializeField] private Animator ButtonAnimation;
+    [SerializeField] private PlayableDirector director;
+
+    [Header("Тригер кнопки")]
+    [SerializeField] private Collider triggerZone;
+    [SerializeField] private Transform playerTransform;
+
+    [Header("Скрипты")]
+    [SerializeField] private TeleportOnKey TeleportOnKeyPlayer;
+    [SerializeField] private PlayerCamera PlayerCameraScript;
+    [SerializeField] private Player PlayerMovementScript;
+
+    [Header("Для затемнения экрана")]
+    [SerializeField] private CameraFade CameraFade;
+
+    [Header("Камера Анимации")]
+    [SerializeField] private GameObject Camera;
+
+    [Header("Двери")]
+    [SerializeField] private GameObject AnimationDoor;
+    [SerializeField] private GameObject RealDoor;
+
+    [Header("Монстр")]
+    [SerializeField] private GameObject FakeMonster;
+    [SerializeField] private GameObject RealMonster;
+    [SerializeField] private Transform MonsterTransform;
+
+    [Header("Тригер диалога")]
+    [SerializeField] private GameObject DialogTriger;
+
+    [Header("Материалы")]
+    [SerializeField] private Renderer ButtonMaterial;
+    [SerializeField] private Material newMaterial;
+
+    [Header("Звуковые источники")]
+    [SerializeField] private AudioSource AmbientAudioSource;
+    [SerializeField] private AudioSource GeneratorAudioSource;
+
+    [Header("Звуки")]
+    [SerializeField] private AudioClip AmbientAudio;
+    [SerializeField] private AudioClip GeneratorAudio;
+
+    [Header("Стены для отключения")]
+
+    [SerializeField] private List<GameObject> VisibleWalls;
+
+    [Header("Переменые")]
+    public bool CatScene = false;
+
+    public GameObject ActivateDoorTriger;
 
     public bool IsPlayerInside { get; private set; }
-
-    public bool CatScene = false;
 
     Vector3 RunPos = new Vector3(14.80115f, -2.538255f, -50.82444f);
 
@@ -55,6 +86,7 @@ public class ActivateGenerator : MonoBehaviour
 
     public IEnumerator CatSceneLogic()
     {
+        ActivateDoorTriger.SetActive(true);
         PlayerCameraScript.canMove = false;
         PlayerMovementScript.canMove = false;
         CameraFade.FadeIn();
@@ -65,8 +97,8 @@ public class ActivateGenerator : MonoBehaviour
         Camera.SetActive(true);
         CameraFade.FadeOut();
         yield return new WaitForSeconds(1);
-        ButtonAnimation.SetBool("ButtonPresed", true);
-        ButtonMaterial.material = newMaterial;
+        
+        
         TeleportOnKeyPlayer.Teleport(RunPos, 110.2f);
         yield return new WaitForSeconds(1);
         director.stopped += OnDirectorStopped;
@@ -89,6 +121,25 @@ public class ActivateGenerator : MonoBehaviour
         PlayerCameraScript.canMove = true;
         PlayerMovementScript.canMove = true;
         RealMonster.SetActive(true);
+
+        AmbientAudioSource.volume = 1;
+        AmbientAudioSource.clip = AmbientAudio;
+        AmbientAudioSource.Play();
+    }
+
+    public IEnumerator DialogLogic()
+    {
+        ButtonAnimation.SetBool("ButtonPresed", true);
+        yield return new WaitForSeconds(0.30f);
+        ButtonMaterial.material = newMaterial;
+        yield return new WaitForSeconds(0.50f);
+        GeneratorAudioSource.clip = GeneratorAudio;
+        GeneratorAudioSource.Play();
+    }
+
+    public void StartDialogLogic()
+    {
+        StartCoroutine(DialogLogic());
     }
 
     public void StartCoroutineCatScene()
