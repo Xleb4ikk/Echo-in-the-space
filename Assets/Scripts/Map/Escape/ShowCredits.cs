a5165bca15510357c7cbde3aa53b30c7cf6fa690
Xleb <129383981+Xleb4ikk@users.noreply.github.com>
2025-06-17 00:45:35 +0300

    vbs dg dgggggggggggg

---
+++ Assets/Scripts/Map/Escape/ShowCredits.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class ShowTheEnd : MonoBehaviour
+{
+    public GameObject blackScreen;  // Чёрный экран (Image)
+    public TMP_Text endText;            // Текст "The END"
+    public float displayDuration = 5f; // Время отображения экрана
+
+    // Функция, вызываемая кнопкой
+    public void ShowEndScreen()
+    {
+        StartCoroutine(ShowEndCoroutine());
+    }
+
+    private IEnumerator ShowEndCoroutine()
+    {
+        // Включить чёрный экран и текст
+        blackScreen.SetActive(true);
+        endText.gameObject.SetActive(true);
+
+        // Пауза на экране
+        yield return new WaitForSeconds(displayDuration);
+
+        // Оставьте экран или выполните другие действия (если нужно, отключите экран)
+        // blackScreen.SetActive(false);
+        // endText.gameObject.SetActive(false);
+    }
+}
